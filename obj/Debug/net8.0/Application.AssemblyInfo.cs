b4736c9945f38032d50b4c1b2aebf336669e182e
SamhithThatikonda <st1106@scarletmail.rutgers.edu>
2024-08-05 12:42:28 -0400

    chore: Deleted all the unwanted comments
     Add cascading delete functionality for Employee and Department
    
    This commit adds cascading delete functionality for the `DeleteEmployee` and `DeleteDept` features. When an employee or department is deleted, all related data in associated tables is also removed, ensuring data integrity.

--- obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c974339481f208cb1f7d3c400019c451f07f1470")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7edbc42c38bcb4859cdd6f24484f7d00778018d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
