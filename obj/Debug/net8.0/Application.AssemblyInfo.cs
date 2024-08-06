44542f239ee12fd802d527bca8db57279af56fac
SamhithThatikonda <st1106@scarletmail.rutgers.edu>
2024-08-06 15:03:42 -0400

    Added Pager Model

--- obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb9c43b413a5f73c71ef7abdffa0f89aafe338e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31c67957b72427c32e4d6a727264e7a374d23eb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
