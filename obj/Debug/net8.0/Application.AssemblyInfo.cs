67fe5513aaca6e05f18833b694f30805f4887389
SamhithThatikonda <st1106@scarletmail.rutgers.edu>
2024-08-07 10:37:17 -0400

    Stashed code

--- obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+203cf32a35f309be13c88e243d42171389bc8ffb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ac09f64ef460e36163f4857ede8ff2587651906")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
